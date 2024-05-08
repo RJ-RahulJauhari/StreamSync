ee54558fd0dc9baddd8f7ebcd630ad862f9b65b1
Rahul Jauhari <rj.rahul.jauhari@gmail.com>
2024-05-09 01:54:49 +0530

    Added Website Icon

--- app/layout.js
+++ app/layout.js
@@ -15,6 +15,9 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <head>
+        <link rel="shortcut icon" href="/icons/logo.png" />
+      </head>
       <ClerkProvider appearance={{
           variables:{
             colorText:"#fff",
