86f0f3abfe27271c3b5ab84688bb2fb93f113090
Rahul Jauhari <rj.rahul.jauhari@gmail.com>
2024-05-11 16:17:19 +0530

    Some corrections

--- components/MeetingCard.jsx
+++ components/MeetingCard.jsx
@@ -1,10 +1,11 @@
+// "use client"
 import React from 'react'
 import Image from "next/image";
 
 import { cn } from "@/lib/utils";
 import { Button } from "./ui/button";
 import { avatarImages } from "@/constants";
-import { useToast } from "./ui/use-toast";
+import { toast } from './ui/use-toast';
 
 const MeetingCard = ({
     icon,
